61dfe6d239e24fed12f9247ea70c98c3bfbe976a
Brandon Bayer <b@bayer.ws>
2019-12-16 17:16:04 -0500

    Revert today's commits

--- api/v1/graphql.ts
+++ api/v1/graphql.ts
@@ -1,16 +1,21 @@
-import { ApolloServer } from "apollo-server-micro"
+import { ApolloServer, AuthenticationError } from "apollo-server-micro"
+import { GraphQLClient } from "graphql-request"
 import { importSchema } from "graphql-import"
 import path from "path"
-
-import { getAdminSdk } from "../../graphql-clients/platform"
+import { getSdk } from "../../graphql-clients/platform/sdk.generated"
 import resolvers from "./_graphql/resolvers"
-import { authenticateIpc } from "../_utils/auth"
 
 const schema = path.join(__dirname, "./_graphql/schema.graphql")
 
+if (process.env.NODE_ENV === "production") {
+  if (!process.env.HASURA_GRAPHQL_ADMIN_SECRET)
+    throw new Error("You must set the HASURA_GRAPHQL_ADMIN_SECRET env variable")
+  if (!process.env.ENCRYPTION_KEY) throw new Error("You must set the ENCRYPTION_KEY env variable")
+}
+
 export interface Context {
   authRole: string
-  adminSdk: ReturnType<typeof getAdminSdk>
+  adminSdk: ReturnType<typeof getSdk>
 }
 
 const server = new ApolloServer({
@@ -19,11 +24,26 @@ const server = new ApolloServer({
   introspection: true,
   playground: process.env.NODE_ENV === "development",
   context: ({ req }): Context => {
-    authenticateIpc(req)
+    const keyHeader = req.headers["x-hasura-admin-secret"]
+    // Ensure request is coming from Hasura
+    if (
+      process.env.NODE_ENV === "production" &&
+      (!keyHeader || keyHeader !== process.env.HASURA_GRAPHQL_ADMIN_SECRET)
+    ) {
+      throw new AuthenticationError("You must be logged in")
+    }
+
+    // const client = new GraphQLClient("http://localhost:8080/v1/graphql")
+    const client = new GraphQLClient(process.env.HASURA_URL as string, {
+      headers: {
+        "x-hasura-admin-secret": process.env.HASURA_GRAPHQL_ADMIN_SECRET as string,
+      },
+    })
+    const adminSdk = getSdk(client)
 
     return {
       authRole: req.headers["x-hasura-role"],
-      adminSdk: getAdminSdk(),
+      adminSdk,
     }
   },
 })
