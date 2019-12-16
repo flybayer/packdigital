a43cb4296400de5d597d044366b6cb3e612c2761
Brandon Bayer <b@bayer.ws>
2019-12-16 16:33:43 -0500

    wip

---
+++ graphql-clients/platform/index.ts
@@ -0,0 +1,17 @@
+import { GraphQLClient } from "graphql-request"
+import { getSdk } from "./sdk.generated"
+
+if (process.env.NODE_ENV === "production") {
+  if (!process.env.HASURA_GRAPHQL_ADMIN_SECRET) {
+    throw new Error("You must set the HASURA_GRAPHQL_ADMIN_SECRET env variable")
+  }
+}
+
+export function getAdminSdk() {
+  const client = new GraphQLClient(process.env.HASURA_URL as string, {
+    headers: {
+      "x-hasura-admin-secret": process.env.HASURA_GRAPHQL_ADMIN_SECRET as string,
+    },
+  })
+  return getSdk(client)
+}
